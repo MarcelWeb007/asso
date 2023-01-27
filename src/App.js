111da4eef5bbaf9fb6adf15898ba30c829016efd
ANVANE Marcel <53498389+MarcelWeb007@users.noreply.github.com>
2023-01-27 18:06:59 +0100

    base

---
+++ src/App.js
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div>
+      <i className="bi bi-2-circle-fill"></i>
+      <button type="button" className="btn btn-primary">Primary</button>
+    </div>
+  );
+}
+
+export default App;
